5dc36a62bb60c014ef88647ff9b81ab9d4a268b5
hyeung <debouncing@gmail.com>
2010-01-31 19:07:15 +1100

    Trying to fix the provisioning profile errors in build script.

--- build/build.sh
+++ build/build.sh
@@ -11,13 +11,8 @@ set -ex
 export OUTPUT=$WORKSPACE/output
 rm -rf $OUTPUT
 mkdir -p $OUTPUT
-PROFILE_HOME=~/Library/MobileDevice/Provisioning\ Profiles/
-KEYCHAIN=~/Library/Keychains/login.keychain
 
 . "$WORKSPACE/build/build.config"
-
-[ -d "$PROFILE_HOME" ] || mkdir -p "$PROFILE_HOME"
-security unlock -p $PASSWORD
 
 cd $WORKSPACE/iphone;
 agvtool new-version -all $BUILD_NUMBER
